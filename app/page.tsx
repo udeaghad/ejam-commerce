b0eb5f42a29c2999ac01ba941a87dc5d9f796a29
udeaghad <dozie.udeagha@gmail.com>
2023-09-15 11:37:36 +0100

    Create mobile NavBar component

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-import Image from 'next/image'
+import NavBar from "@/components/NavBar/NavBar"
 
 export default function Home() {
   return (
     <main>
-      <h1 className='text-pry-red'>Home 53</h1>
-      <Image src='/images/clarifion_logo_1.png' alt='logo' width={100} height={100} />
+      <NavBar />
     </main>
   )
 }
---
+++ components/NavBar/NavBar.tsx
@@ -0,0 +1,20 @@
+import { MdOutlineArrowBackIos } from 'react-icons/md';
+import { MdArrowForwardIos } from 'react-icons/md';
+import { BsPatchCheck } from 'react-icons/bs';
+
+const NavBar = () => {
+  return (
+    <div className="px-2.5 py-5 w-full bg-main-blue">
+      <div className="h-5 w-full flex justify-between items-center">
+        <MdOutlineArrowBackIos className="text-[#ffffff] text-xl"/>
+        <div className='flex justify-center items-center gap-2'>
+          <BsPatchCheck className="text-[#ffffff] text-xl"/>
+          <h2 className="text-xs font-medium text-[#ffffff]">30-DAY SATISFACTION GUARANTEE</h2>
+        </div>
+        <MdArrowForwardIos className="text-[#ffffff] text-xl"/>
+      </div>
+    </div>
+  )
+}
+
+export default NavBar
