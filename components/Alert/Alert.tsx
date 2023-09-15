e9412dd3e2fadf0344392fcf331886217b7bd5e1
udeaghad <dozie.udeagha@gmail.com>
2023-09-16 00:55:28 +0100

    Update Alert for responsiveness

--- components/Alert/Alert.tsx
+++ components/Alert/Alert.tsx
@@ -1,8 +1,8 @@
 const Alert = () => {
   return (
     <div className="flex flex-col gap-4 w-full">
-      <h1 className="text-[2rem] font-normal capitalize text-center">Wait ! your order in progress.</h1>
-      <p className="text-base font-normal capitalize text-center">Lorem ipsum dolor sit amet, consectetur </p>
+      <h1 className="text-[2rem] font-normal capitalize text-center md:text-5xl">Wait ! your order in progress.</h1>
+      <p className="text-base font-normal capitalize text-center md:text-2xl">Lorem ipsum dolor sit amet, consectetur </p>
     </div>
   )
 }
