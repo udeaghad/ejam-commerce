61970c89e2ee1e4615f0a23efa2dc616f415d0e5
udeaghad <dozie.udeagha@gmail.com>
2023-09-15 12:18:42 +0100

    Update Alert component

--- components/Alert/Alert.tsx
+++ components/Alert/Alert.tsx
@@ -1,7 +1,8 @@
 const Alert = () => {
   return (
-    <div className="px-5">
+    <div className="px-5 flex flex-col gap-6 w-full">
       <h1 className="text-[2rem] font-normal">Wait ! your order in progress.</h1>
+      <p className="text-base font-normal">Lorem ipsum dolor sit amet, consectetur </p>
     </div>
   )
 }
