3d30cf04daddc451a2eca3ccd245fd6d3aa13580
udeaghad <dozie.udeagha@gmail.com>
2023-09-16 01:40:53 +0100

    Update PromoAlert for responsiveness

--- components/PromoAlert/PromoAlert.tsx
+++ components/PromoAlert/PromoAlert.tsx
@@ -6,7 +6,7 @@ const PromoAlert = () => {
           %
         </div>
 
-        <p className="text-sm">
+        <p className="text-sm md:text-base">
           Save <span className="text-pry-blue font-normal">53%</span> and get <span className="text-pry-blue">6 extra Clarifision</span> for only <span className="text-pry-blue">$14 Each</span>.
         </p>
       </div>
