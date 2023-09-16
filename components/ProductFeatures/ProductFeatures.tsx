dd21758905d52fe071fe8af10ce74d265a8ac42f
udeaghad <dozie.udeagha@gmail.com>
2023-09-16 01:37:33 +0100

    Update ProductFeatures component for responsiveness

--- components/ProductFeatures/ProductFeatures.tsx
+++ components/ProductFeatures/ProductFeatures.tsx
@@ -4,20 +4,20 @@ const ProductFeatures = () => {
   return (
     <div className='flex flex-col justify-start items-start gap-4 w-full'>
       <div className='flex justify-start items-center gap-3'>
-        <BsCheckLg className="text-base text-pry-blue"/>
-        <p className='text-xs text-[#4D5254]'>
+        <BsCheckLg className="text-base text-pry-blue md:text-[1.375rem]"/>
+        <p className='text-xs text-pry-gray md:text-base'>
           Negative Ion Technology may <span className='font-bold'>help with allergens</span>
         </p>
       </div>
       <div className='flex justify-start items-center gap-3'>
-        <BsCheckLg className="text-base text-pry-blue"/>
-        <p className='text-xs text-[#4D5254]'>
+        <BsCheckLg className="text-base text-pry-blue md:text-[1.375rem]"/>
+        <p className='text-xs text-pry-gray md:text-base'>
           Designed for <span className='font-bold'>air rejuvenation</span>
         </p>
       </div>
       <div className='flex justify-start items-center gap-3'>
-        <BsCheckLg className="text-base text-pry-blue"/>
-        <p className='text-xs text-[#4D5254]'>
+        <BsCheckLg className="text-base text-pry-blue md:text-[1.375rem]"/>
+        <p className='text-xs text-pry-gray md:text-base'>
           <span className='font-bold'>Perfect for every room</span> in all types of places.
         </p>
       </div>
