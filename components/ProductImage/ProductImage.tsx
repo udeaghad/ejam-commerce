0b5e1865c8665c9f5dd4e36dcd28d34cec864b72
udeaghad <dozie.udeagha@gmail.com>
2023-09-16 01:12:52 +0100

    Update Product image component for responsiveness

--- components/ProductImage/ProductImage.tsx
+++ components/ProductImage/ProductImage.tsx
@@ -1,6 +1,6 @@
 const ProductImage = () => {
   return (
-    <div className="w-full mx-2.5 flex justify-center items-center rounded-2xl border-0 h-[20.5rem] bg-gray-300">
+    <div className="w-full mx-2.5 flex justify-center items-center rounded-2xl border-0 h-[20.5rem] md:h-[36rem]">
       <img src='/images/product-image-1.png' alt="product-image" className="top-0 left-0 right-0 bottom-0" />
     </div>
   )
